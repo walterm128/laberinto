73a882200f3e1906360890e1eb2e8326c20a6578
Walter Marcelo <47644901+walterm128@users.noreply.github.com>
2023-11-04 00:56:55 -0300

    Update

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1699043239|1644229564";
+const CACHE_VERSION = "1699070174|8115057535";
 const CACHE_PREFIX = "Laberinto-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
